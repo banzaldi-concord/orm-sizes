57b750282ea08a18f4205d885c6737e7a4d403e5
banzaldi-concord <120408328+banzaldi-concord@users.noreply.github.com>
2023-09-01 21:01:51 -0700

    Prisma benchmark

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world!');
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+}
+
+main().then(() => {}).catch(console.error).finally(async () => {
+  await prisma.$disconnect();
+})
